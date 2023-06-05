f81c4332f7a4df1c6f2a7541cde3b89d37f4e185
Sander Revenberg <sander.revenberg@ing.com>
2023-06-05 21:35:11 +0200

    .....

--- custom_components/s05channel/sensor.py
+++ custom_components/s05channel/sensor.py
@@ -198,13 +198,10 @@ class S05ChannelSN(S05ChannelSensorBase):
 class S05ChannelPort(S05ChannelSensorBase):
     """S05ChannelPort."""
 
-    #device_class = SensorDeviceClass.ENERGY
-    #state_class = SensorStateClass.TOTAL_INCREASING
-    #native_unit_of_measurement = UnitOfEnergy.KILO_WATT_HOUR
-
-    device_class = SensorDeviceClass.POWER
+    device_class = SensorDeviceClass.ENERGY
+#    state_class = SensorStateClass.TOTAL_INCREASING
     state_class = SensorStateClass.MEASUREMENT
-    native_unit_of_measurement = UnitOfPower.ENERGY_KILO_WATT_HOUR
+    native_unit_of_measurement = UnitOfEnergy.KILO_WATT_HOUR
 
     suggested_display_precision = 0
 
